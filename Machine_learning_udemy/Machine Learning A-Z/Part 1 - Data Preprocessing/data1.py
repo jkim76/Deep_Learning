beaec0bff81d4f3110e74ce048657381b6a0a2e5
JUNGWOO KIM <jkim76@berklee.edu>
2019-09-27 15:25:39 -0400

    first commit

---
+++ Machine_learning_udemy/Machine Learning A-Z/Part 1 - Data Preprocessing/data1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#importing a library
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+import os
+
---
+++ Machine_learning_udemy/Machine Learning A-Z/Part 1 - Data Preprocessing/data_1.py
@@ -0,0 +1,6 @@
+#importing a library
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Data.csv')
